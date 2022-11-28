0820cb9eedcd9359a20a75c752ea87f58a5ae4ed
satya mukkavilli <satyasmukkavilli@gmail.com>
2022-11-28 15:51:15 -0500

    Created a class Card with suits and values in it

--- src/main/java/edu/midlands/training/Card.java
+++ src/main/java/edu/midlands/training/Card.java
@@ -1,4 +1,13 @@
 package edu.midlands.training;
+import java.util.ArrayList;
 
 public class Card {
-}
+
+        public String suit,value; // Creating a card class which takes a suit and a value of the card
+
+        public Card(String suit, String value) { // Generating constructors for this object
+            setSuit(suit);
+            setValue(value);
+        }
+
+    }
