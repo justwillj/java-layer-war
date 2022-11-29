834a69f58f0b693cd3f10e53a5a54a72389cf1c9
Justin Williams <williams.jman@gmail.com>
2022-11-29 12:56:51 -0500

    Grabs the rank and the suit of the current cards that are being played

--- src/main/java/edu/midlands/training/Card.java
+++ src/main/java/edu/midlands/training/Card.java
@@ -53,6 +53,13 @@ public class Card {
         return value;
     }
 
+    public void setSuit(String suit){
+        this.suit = suit;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
     public String toString(){
         return rank + " of " + suit;
     }
