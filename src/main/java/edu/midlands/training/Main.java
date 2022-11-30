83050760547d48958d4ad733ec6dc80596db49a0
“Patrick <patrickfordham@student.midlandstech.edu>
2022-11-30 13:46:20 -0500

    testing war logic

--- src/main/java/edu/midlands/training/Main.java
+++ src/main/java/edu/midlands/training/Main.java
@@ -46,21 +46,62 @@ public class Main {
                     System.out.println("Player2 wins!");
                     System.out.println("player 1 deck size " + deckOne.size());
                     System.out.println("player 2 deck size " + deckTwo.size());
-                } else if (p1.getValue() == p2.getValue()) {
-                    deckTwo.add(p2);
-                    deckTwo.add(p1);
-                    System.out.println("Player 2 wins the WAR");
-                    System.out.println("player 1 deck size " + deckOne.size());
-                    System.out.println("player 2 deck size " + deckTwo.size());
+                } else { //war happens when both cards' rank matched
+                    System.out.println("War");
 
-                    }
+                    //creating war cards
+                    List<Card> warDeckOne = new ArrayList<Card>();
+                    List<Card> warDeckTwo = new ArrayList<Card>();
+
+                    //checking do players have enough (4)cards to stay in game
+                    for(int x=0; x<3; x++){
+                        //either one player runs out of card is game over
+                        if(deckOne.size() == 0){//change if to say who ran out of card
+                            System.out.println("Player 1 out of cards. Player 2 Wins");
+                            break;
+                        }else if (deckTwo.size() == 0 ){
+                            System.out.println("Player 2 out of cards. Player 1 Wins");
+                            break;
+                        }//end if
+
+                        System.out.println("War card for player 1 is x\nWar card for player 2 is x");
+
+                        warDeckOne.add(deckOne.remove(0));  //place additional card for war
+                        warDeckTwo.add(deckTwo.remove(0));
+                    }//end for
+
+                    //only compare result when both players have enough cards for war
+                    if(warDeckOne.size() == 3 && warDeckTwo.size() == 3 ){
+                        //display the war cards from each player
+                        System.out.println("War card for player1 is " + warDeckOne.get(0).toString());
+                        System.out.println("War card for player2 is " + warDeckTwo.get(0).toString());
+
+                        //if player 1 wins the war round
+                        if(warDeckOne.get(2).getValue() > warDeckTwo.get(2).getValue()){
+                            deckOne.addAll(warDeckOne); //player1 get all 10 cards
+                            deckOne.addAll(warDeckTwo);
+                            System.out.println("Player One Wins The War Round");
+//                            System.out.println("Player one deck " + deck1.size());
+//                            System.out.println("Player two deck " + deck2.size());
+                        }//end if
+                        //otherwise player 2 wins the war round
+                        else{
+                            deckTwo.addAll(warDeckOne); //player2 get all 10 cards
+                            deckTwo.addAll(warDeckTwo);
+                            System.out.println("Player Two Wins The War Round");
+//                            System.out.println("Player one deck " + deck1.size());
+//                            System.out.println("Player two deck " + deck2.size());
+                        }//end else
+                    }//end if
+
+                }//end war round else
 
                if(deckOne.size() == 0){
-                    System.out.println("Player2 Wins the game");
+                    System.out.println("Player 2 Wins the game");
                     break;
                 }
                 else if (deckTwo.size() == 0){
-                    System.out.println("Player1 Wins the game");
+                    System.out.println("Player 1 Wins the game");
                     break;
                 }
 
