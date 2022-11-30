263284992adf83ecee74a09a55b028f325b57a63
Justin Williams <williams.jman@gmail.com>
2022-11-30 13:47:25 -0500

    Added some method for the war

--- src/main/java/edu/midlands/training/Main.java
+++ src/main/java/edu/midlands/training/Main.java
@@ -15,15 +15,22 @@ public class Main {
                 deckOfCards.add( new Card(Suits[i],ranks[j]));
             }
         }
+
+
         //Shuffles the deck of cards before we spilt it in half
         Collections.shuffle(deckOfCards);
 
         ArrayList<Card>deckOne = new ArrayList<Card>();
         ArrayList<Card>deckTwo = new ArrayList<Card>();
+
+
         deckOne.addAll(deckOfCards.subList(0, 26));
         deckTwo.addAll(deckOfCards.subList(26, deckOfCards.size()));
         System.out.println(deckOne.size());
         System.out.println(deckTwo.size());
+        int count = 0;
+
+        Card war1 = new Card();
 
 
             while (true) {
@@ -46,10 +53,51 @@ public class Main {
                     System.out.println("Player2 wins!");
                     System.out.println("player 1 deck size " + deckOne.size());
                     System.out.println("player 2 deck size " + deckTwo.size());
+
                 } else if (p1.getValue() == p2.getValue()) {
-                    deckTwo.add(p2);
-                    deckTwo.add(p1);
-                    System.out.println("Player 2 wins the WAR");
+                    System.out.println(deckOne.size());
+                    System.out.println(deckTwo.size());
+
+                    if(deckOne.size() < 4){
+                        System.out.println("Player 2 wins the game!");
+                        break;
+                    } else if(deckTwo.size() <4){
+                        System.out.println("Player 1 wins the game!");
+                        break;
+                    } else if(deckOne.get(3).getValue()>deckTwo.get(3).getValue()){
+                        //Players 1 wins
+                        Card lossPlayer21 = deckTwo.remove(0);
+                        Card lossPlayer22 = deckTwo.remove(1);
+                        Card lossPlayer23 = deckTwo.remove(2);
+                        Card lossPlayer24 = deckTwo.remove(3);
+
+                        deckOne.add(lossPlayer21);
+                        deckOne.add(lossPlayer22);
+                        deckOne.add(lossPlayer23);
+                        deckOne.add(lossPlayer24);
+
+                        System.out.println("Player 1 wins the war!");
+
+                    } else if (deckOne.get(3).getValue()<deckTwo.get(3).getValue()) {
+
+                        Card lossPlayer21 = deckOne.remove(0);
+                        Card lossPlayer22 = deckOne.remove(1);
+                        Card lossPlayer23 = deckOne.remove(2);
+                        Card lossPlayer24 = deckOne.remove(3);
+
+                        deckTwo.add(lossPlayer21);
+                        deckTwo.add(lossPlayer22);
+                        deckTwo.add(lossPlayer23);
+                        deckTwo.add(lossPlayer24);
+
+
+                        System.out.println("Player 2 wins the war!");
+
+                    } else if (deckOne.get(3).getValue()==deckTwo.get(3).getValue()){
+                        System.out.println("Test");
+                    }
+
+
                     System.out.println("player 1 deck size " + deckOne.size());
                     System.out.println("player 2 deck size " + deckTwo.size());
 
@@ -67,6 +115,7 @@ public class Main {
             }//end while
         System.out.println(deckOne.size());
         System.out.println(deckTwo.size());
+
     }
 
 }
