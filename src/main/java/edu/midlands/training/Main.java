266c19a8a7134ea6a271ac7954c591f7a231988f
Justin Williams <williams.jman@gmail.com>
2022-11-30 15:11:05 -0500

    Added the correct print statements inisde of the console

--- src/main/java/edu/midlands/training/Main.java
+++ src/main/java/edu/midlands/training/Main.java
@@ -7,115 +7,184 @@ public class Main {
         //Link to a resources that we found helpful
         //https://books.trinket.io/thinkjava/chapter12.html
         ArrayList<Card> deckOfCards = new ArrayList<>();
+        ArrayList<Card> matching = new ArrayList<Card>();
         String[] Suits = {"Hearts", "Spades", "Clubs", "Diamonds"};
         String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
         for (int i = 0; i < Suits.length; i++) {
             for (int j = 0; j < ranks.length; j++) {
-                deckOfCards.add( new Card(Suits[i],ranks[j]));
+                deckOfCards.add(new Card(Suits[i], ranks[j]));
             }
         }
-
-
         //Shuffles the deck of cards before we spilt it in half
         Collections.shuffle(deckOfCards);
 
-        ArrayList<Card>deckOne = new ArrayList<Card>();
-        ArrayList<Card>deckTwo = new ArrayList<Card>();
-
-
+        ArrayList<Card> deckOne = new ArrayList<Card>();
+        ArrayList<Card> deckTwo = new ArrayList<Card>();
         deckOne.addAll(deckOfCards.subList(0, 26));
         deckTwo.addAll(deckOfCards.subList(26, deckOfCards.size()));
-        System.out.println(deckOne.size());
-        System.out.println(deckTwo.size());
-        int count = 0;
-
-        Card war1 = new Card();
+        int roundCount = 0;
 
 
-            while (true) {
-                Card p1 = deckOne.remove(0);
-                Card p2 = deckTwo.remove(0);
+        while (true) {
+            Card p1 = deckOne.remove(0);
+            Card p2 = deckTwo.remove(0);
 
-                System.out.println("Player 1: " + p1);
-                System.out.println("Player 2: " + p2);
+            System.out.println("Round " +roundCount + " of The War");
+            System.out.println("Player 1: " + p1);
+            System.out.println("Player 2: " + p2);
+            if (p1.getValue() > p2.getValue()) {
+                deckOne.add(p2);
+                deckOne.add(p1);
+                System.out.println("Player 1 wins the war!");
+                roundCount++;
+            } else if (p1.getValue() < p2.getValue()) {
+                deckTwo.add(p1);
+                deckTwo.add(p2);
+                System.out.println("Player 2 wins the war!");
+                roundCount++;
 
-                if (p1.getValue() > p2.getValue()) {
-                    deckOne.add(p2);
-                    deckOne.add(p1);
-                    System.out.println("Player1 wins!");
-                    System.out.println("player 1 deck size " + deckOne.size());
-                    System.out.println("player 2 deck size " + deckTwo.size());
-                }
-                else if (p1.getValue() < p2.getValue()){
-                    deckTwo.add(p1);
-                    deckTwo.add(p2);
-                    System.out.println("Player2 wins!");
-                    System.out.println("player 1 deck size " + deckOne.size());
-                    System.out.println("player 2 deck size " + deckTwo.size());
+            } else if (p1.getValue() == p2.getValue()) {
+                if (deckOne.size() >= 4 && deckTwo.size() >= 4) {
+                    Card p1card1 = deckOne.remove(0);
+                    Card p1card2 = deckOne.remove(0);
+                    Card p1card3 = deckOne.remove(0);
+                    Card p1card4 = deckOne.remove(0);
 
-                } else if (p1.getValue() == p2.getValue()) {
-                    System.out.println(deckOne.size());
-                    System.out.println(deckTwo.size());
+                    Card p2card1 = deckTwo.remove(0);
+                    Card p2card2 = deckTwo.remove(0);
+                    Card p2card3 = deckTwo.remove(0);
+                    Card p2card4 = deckTwo.remove(0);
 
-                    if(deckOne.size() < 4){
-                        System.out.println("Player 2 wins the game!");
-                        break;
-                    } else if(deckTwo.size() <4){
-                        System.out.println("Player 1 wins the game!");
-                        break;
-                    } else if(deckOne.get(3).getValue()>deckTwo.get(3).getValue()){
-                        //Players 1 wins
-                        Card lossPlayer21 = deckTwo.remove(0);
-                        Card lossPlayer22 = deckTwo.remove(1);
-                        Card lossPlayer23 = deckTwo.remove(2);
-                        Card lossPlayer24 = deckTwo.remove(3);
-
-                        deckOne.add(lossPlayer21);
-                        deckOne.add(lossPlayer22);
-                        deckOne.add(lossPlayer23);
-                        deckOne.add(lossPlayer24);
-
-                        System.out.println("Player 1 wins the war!");
-
-                    } else if (deckOne.get(3).getValue()<deckTwo.get(3).getValue()) {
-
-                        Card lossPlayer21 = deckOne.remove(0);
-                        Card lossPlayer22 = deckOne.remove(1);
-                        Card lossPlayer23 = deckOne.remove(2);
-                        Card lossPlayer24 = deckOne.remove(3);
-
-                        deckTwo.add(lossPlayer21);
-                        deckTwo.add(lossPlayer22);
-                        deckTwo.add(lossPlayer23);
-                        deckTwo.add(lossPlayer24);
+                    if (p1card4.getValue() > p2card4.getValue()) //if player1 wins
+                    {
+                        deckOne.add(p1);
+                        deckOne.add(p2);
+                        deckOne.add(p1card1);
+                        deckOne.add(p1card2);
+                        deckOne.add(p1card3);
+                        deckOne.add(p1card4);
+                        deckOne.add(p2card1);
+                        deckOne.add(p2card2);
+                        deckOne.add(p2card3);
+                        deckOne.add(p2card4);
+                        System.out.println("Player 1 wins the war");
+                        roundCount++;
 
 
-                        System.out.println("Player 2 wins the war!");
+                    } else if (p2card4.getValue() > p1card4.getValue()) {
+                        deckTwo.add(p1);
+                        deckTwo.add(p2);
+                        deckTwo.add(p1card1);
+                        deckTwo.add(p1card2);
+                        deckTwo.add(p1card3);
+                        deckTwo.add(p1card4);
+                        deckTwo.add(p2card1);
+                        deckTwo.add(p2card2);
+                        deckTwo.add(p2card3);
+                        deckTwo.add(p2card4);
+                        System.out.println("Player 2 wins the war");
+                        roundCount++;
+                    } else if (p2card4.getValue() == p1card4.getValue()){
+                        deckOne.add(p1);
+                        deckTwo.add(p2);
+                        deckOne.add(p1card1);
+                        deckOne.add(p1card2);
+                        deckOne.add(p1card3);
+                        deckOne.add(p1card4);
+                        deckTwo.add(p2card1);
+                        deckTwo.add(p2card2);
+                        deckTwo.add(p2card3);
+                        deckTwo.add(p2card4);
 
-                    } else if (deckOne.get(3).getValue()==deckTwo.get(3).getValue()){
-                        System.out.println("Test");
                     }
 
+                } else if (deckOne.size() < 4) {
+                    System.out.println("Player 2 wins the war");
 
-                    System.out.println("player 1 deck size " + deckOne.size());
-                    System.out.println("player 2 deck size " + deckTwo.size());
 
-                    }
 
-               if(deckOne.size() == 0){
-                    System.out.println("Player2 Wins the game");
-                    break;
+                } else if (deckTwo.size() < 4) {
+                    System.out.println("Player 1 wins the war");
                 }
-                else if (deckTwo.size() == 0){
-                    System.out.println("Player1 Wins the game");
-                    break;
-                }
+            }
 
-            }//end while
+            if (deckOne.size() == 0) {
+                System.out.println("Player 2 Wins the game");
+                break;
+            } else if (deckTwo.size() == 0) {
+                System.out.println("Player 1 Wins the game");
+                break;
+            }
+
+
+        }
+
+
+//    public static  void war(ArrayList<Card>player1,ArrayList<Card>player2) {
+//        ArrayList<Card> deckOfCards = new ArrayList<>();
+//        String[] Suits = {"Hearts", "Spades", "Clubs", "Diamonds"};
+//        String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+//
+//        for (int i = 0; i < Suits.length; i++) {
+//            for (int j = 0; j < ranks.length; j++) {
+//                deckOfCards.add(new Card(Suits[i], ranks[j]));
+//            }
+//        }
+//        ArrayList<Card> matching = new ArrayList<Card>();
+//        ArrayList<Card> deckOne = new ArrayList<Card>();
+//        ArrayList<Card> deckTwo = new ArrayList<Card>();
+//        deckOne.addAll(deckOfCards.subList(0, 26));
+//        deckTwo.addAll(deckOfCards.subList(26, deckOfCards.size()));
+//        Card p1card1 = deckOne.remove(0);
+//        Card p1card2 = deckOne.remove(0);
+//        Card p1card3 = deckOne.remove(0);
+//        Card p1card4 = deckOne.remove(0);
+//
+//        Card p2card1 = deckTwo.remove(0);
+//        Card p2card2 = deckTwo.remove(0);
+//        Card p2card3 = deckTwo.remove(0);
+//        Card p2card4 = deckTwo.remove(0);
+//
+//        if (p1card4.getValue() > p2card4.getValue()) //if player1 wins
+//        {
+//
+//            deckOne.add(p1card1);
+//            deckOne.add(p1card2);
+//            deckOne.add(p1card3);
+//            deckOne.add(p1card4);
+//            deckOne.add(p2card1);
+//            deckOne.add(p2card2);
+//            deckOne.add(p2card3);
+//            deckOne.add(p2card4);
+//
+//
+//        } else if (p2card4.getValue() > p1card4.getValue()) {
+//            deckTwo.add(p1card1);
+//            deckTwo.add(p1card2);
+//            deckTwo.add(p1card3);
+//            deckTwo.add(p1card4);
+//            deckTwo.add(p2card1);
+//            deckTwo.add(p2card2);
+//            deckTwo.add(p2card3);
+//            deckTwo.add(p2card4);
+//        }
+////        else{
+////
+////            war(player1,player2);
+////
+////
+////        }
+//
+////        } else if (p2card4.getValue() == p1card4.getValue()) {
+////            war(player1,player2);
+////        }
+//
+//
+//    }
+
         System.out.println(deckOne.size());
         System.out.println(deckTwo.size());
-
+        System.out.println(matching.size());
     }
-
 }
